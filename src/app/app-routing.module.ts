e08ef75f450b8a8f4f47197407d468b0fd5c621f
icporcincula <porcincula.developer@gmail.com>
2022-08-26 10:33:11 +0800

    Add route for reset password component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IndexComponent } from './index/index.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'home', component: DashboardComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
+  { path: 'auth/password/reset', component: ResetPasswordComponent },
 ];
 
 @NgModule({
