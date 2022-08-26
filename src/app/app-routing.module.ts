8b8e55cffb545258d2debdb1cec99c55d271aa72
icporcincula <porcincula.developer@gmail.com>
2022-08-26 10:12:04 +0800

    Add routes for Index, Signin, Signup pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { IndexComponent } from './index/index.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: '/home', component: DashboardComponent },
+  { path: '/signup', component: SignupComponent },
+  { path: '/signin', component: SigninComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
