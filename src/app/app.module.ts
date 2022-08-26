4aaeaf54f6e47dcff97bd83f3a94f5bd63194ce3
icporcincula <porcincula.developer@gmail.com>
2022-08-26 10:09:12 +0800

    Add components for Index, Signup, Signin Pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { IndexComponent } from './index/index.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    IndexComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
